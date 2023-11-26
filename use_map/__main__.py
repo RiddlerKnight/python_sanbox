97460bedce28b8b98fa5b9df8980eb4f6bc7ed2f
Bawornsit.C <puksigon@gmail.com>
2023-11-26 13:35:32 +0700

    add: yell_v2

--- use_map/__main__.py
+++ use_map/__main__.py
@@ -1,14 +1,19 @@
-def yell(*word):
+def yell(*words):
     '''
     User map function to iterate each word and passing to string uppercase
     function and return list of str
     This is more pythonic way
     '''
-    uppercased =  map(str.upper, word)
+    uppercased =  map(str.upper, words)
+    print(*uppercased)
+
+def yell_v2(*words):
+    uppercased = [word.upper() for word in words ]
     print(*uppercased)
 
 def main():
     yell("This", "is", "My", "Map", "Function")
+    yell_v2("This", "is", "My", "Map", "Function V2")
 
 if __name__ == "__main__":
     main()
